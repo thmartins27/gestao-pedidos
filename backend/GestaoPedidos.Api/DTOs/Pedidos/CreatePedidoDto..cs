84af759646e50255bd19b9e5a56042fd91738332
Thiago Martins <thiago.olivmartins@gmail.com>
2026-07-16 23:32:32 -0300

    chore: remove arquivo duplicado com typo no nome (CreatePedidoDto..cs)

--- backend/GestaoPedidos.Api/DTOs/Pedidos/CreatePedidoDto..cs
+++
@@ -1,7 +0,0 @@
-namespace GestaoPedidos.Api.DTOs.Pedidos;
-
-public class CreatePedidoDto
-{
-    public int ClienteId { get; set; }
-    public List<CreatePedidoItemDto> Itens { get; set; } = [];
-}
